47df490c3bb14231ea06c50536724b960e12d72c
lourdesmorenou04-m <lourdesmorenou04@gmail.com>
2025-11-22 20:41:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function checkAnswer(answer) {
+    const feedbackElement = document.getElementById('feedback');
 
+    if (answer === 'sol') {
+        feedbackElement.textContent = '¡Correcto! El Sol es nuestra fuente principal de energía.';
+        feedbackElement.style.color = 'green';
+    } else {
+        feedbackElement.textContent = 'Incorrecto. ¡Sigue explorando!';
+        feedbackElement.style.color = 'red';
+    }
+}
